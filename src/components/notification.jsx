52c9a1e80f304a890d872565c5be626ee63e91e1
praneeth <gp.gunturu@gmail.com>
2025-06-16 17:26:01 -0500

    added styles for the close button and made few styling changes

--- src/components/notification.jsx
+++ src/components/notification.jsx
@@ -1,19 +1,24 @@
 import { AiOutlineCheckCircle, AiOutlineClose, AiOutlineCloseCircle, AiOutlineInfoCircle, AiOutlineWarning } from "react-icons/ai"
 import './notification.css'
 
+const iconStyles = {marginRight: "10px"}
 const icons = {
-    success: <AiOutlineCheckCircle />,
-    info: <AiOutlineInfoCircle />,
-    warning: <AiOutlineWarning />,
-    error: <AiOutlineCloseCircle />
+    success: <AiOutlineCheckCircle style={iconStyles} />,
+    info: <AiOutlineInfoCircle style={iconStyles} />,
+    warning: <AiOutlineWarning style={iconStyles} />,
+    error: <AiOutlineCloseCircle style={iconStyles} />
 }
 
-export default function Notification({type="info", message, onClose}) {
+export default function Notification({type="info", message, onClose=() => {}}) {
     return (
         <div className={`notification ${type}`}>
             {icons[type]}
             {message}
-            <AiOutlineClose color="white" />
+            <AiOutlineClose 
+                color="white" 
+                className="closeButton"
+                onClick={() => onClose()}    
+            />
         </div>
     )
 }
