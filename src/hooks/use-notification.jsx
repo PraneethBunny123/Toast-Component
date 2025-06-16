038e70e112c31dc8b55a54e4e68e1001759dfdfd
praneeth <gp.gunturu@gmail.com>
2025-06-16 18:11:56 -0500

    added a trigger funtion that will take the props and set them to the state and we pass that state as props to the Notification Component

--- src/hooks/use-notification.jsx
+++ src/hooks/use-notification.jsx
@@ -4,9 +4,13 @@ import Notification from "../components/notification";
 export default function useNotification(position='top-right') {
     const [notification, setNotification] = useState(null)
 
+    function triggerNotification(notificationProps) {
+        setNotification(notificationProps)
+    }
+
     const NotificationComponent = notification ? (
         <div>
-            <Notification />
+            <Notification {...notification} />
         </div>
     )
 }
