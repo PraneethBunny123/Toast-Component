7250db2736f13038e50861d053766247c6b4b998
praneeth <gp.gunturu@gmail.com>
2025-06-16 18:44:24 -0500

    setting styles for the positon where the toast messages should appear

--- src/hooks/use-notification.jsx
+++ src/hooks/use-notification.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Notification from "../components/notification";
+import '../components/notification.css'
 
 export default function useNotification(position='top-right') {
     const [notification, setNotification] = useState(null)
@@ -12,7 +13,7 @@ export default function useNotification(position='top-right') {
     }
 
     const NotificationComponent = notification ? (
-        <div>
+        <div className={`${position}`}>
             <Notification {...notification} />
         </div>
     ) : null;
