f02985bb3b9425f1cda3d84bcd39293ff48309cb
praneeth <gp.gunturu@gmail.com>
2025-06-16 19:01:22 -0500

    Added all the trigger buttons and by clicking them the toast messages are poping. But right now the toast messages don't stack up

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,22 @@ export default function App() {
             })}>
                 Trigger Error
             </button>
+            <button onClick={() => triggerNotification({
+                type: "info", 
+                message: "The test was new!", 
+                duration: 3000, 
+                onclose: () => {}
+            })}>
+                Trigger info
+            </button>
+            <button onClick={() => triggerNotification({
+                type: "warning", 
+                message: "The test is warning", 
+                duration: 3000, 
+                onclose: () => {}
+            })}>
+                Trigger warning
+            </button>
             {NotificationComponent}
         </div>
     )
